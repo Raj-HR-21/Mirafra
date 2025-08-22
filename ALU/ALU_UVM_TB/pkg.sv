998d4db5b10dce4f1daa4274834230193805ba10
Raj-HR-21 <rajendrahr21@gmail.com>
2025-08-22 10:49:29 +0530

    Create pkg.sv

---
+++ ALU/ALU_UVM_TB/pkg.sv
@@ -0,0 +1,18 @@
+
+package alu_pkg;
+`include "uvm_pkg.sv"
+`include "uvm_macros.svh"
+	import uvm_pkg::*;
+	`include "seq_item.sv"
+	`include "sequence.sv"
+	`include "sequencer.sv"
+	`include "driver.sv"
+	`include "monitor.sv"
+	`include "agent.sv"
+	`include "scoreboard.sv"
+	`include "subscriber.sv"
+	`include "env.sv"
+	`include "test.sv"
+
+
+endpackage
