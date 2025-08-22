281f42c05d297bde944d96bfbb775d65f6340f27
Raj-HR-21 <rajendrahr21@gmail.com>
2025-08-22 22:07:59 +0530

    Update monitor.sv

--- ALU/ALU_UVM_TB/monitor.sv
+++ ALU/ALU_UVM_TB/monitor.sv
@@ -20,11 +20,24 @@ class alu_monitor extends uvm_monitor;
 		end
 	endfunction
 
+
 	function bit both_op();
-		if(mon_item.MODE == 1)
-			return (mon_item.CMD inside {0,1,2,3,8,9,10});
-		else 
-			return (mon_item.CMD inside {[0:5],12,13});
+		if(mon_item.MODE == 1) begin
+			if (mon_item.CMD inside {0,1,2,3,8,9,10}) begin
+				return 1;
+				end
+			else begin
+				return 0;
+				end
+			end
+		if(mon_item.MODE == 0) begin 
+			if (mon_item.CMD inside {[0:5],12,13}) begin
+				return 1;
+				end
+			else begin
+				return 0;
+			end
+			end
 	endfunction : both_op
 
 	function void capture_in();
@@ -37,7 +50,7 @@ class alu_monitor extends uvm_monitor;
 		mon_item.OPA	= vif.monitor_cb.OPA;
 		mon_item.OPB	= vif.monitor_cb.OPB;
 		mon_item.INP_VALID	= vif.monitor_cb.INP_VALID;
-		`uvm_info("MON :", $sformatf("RESET = %b | CE = %b| MODE = %b| CMD = %0d| INP_VALID = %0d| OPA = %0d| OPB = %0d| CIN = %b", mon_item.RESET, mon_item.CE, mon_item.MODE, mon_item.CMD, mon_item.INP_VALID, mon_item.OPA, mon_item.OPB, mon_item.CIN), UVM_LOW)
+		//`uvm_info("MON :", $sformatf("RESET = %b | CE = %b| MODE = %b| CMD = %0d| INP_VALID = %0d| OPA = %0d| OPB = %0d| CIN = %b", mon_item.RESET, mon_item.CE, mon_item.MODE, mon_item.CMD, mon_item.INP_VALID, mon_item.OPA, mon_item.OPB, mon_item.CIN), UVM_LOW)
 	endfunction
 	function void capture_out();
 		//Outputs
@@ -55,45 +68,52 @@ class alu_monitor extends uvm_monitor;
 	task run_phase(uvm_phase phase);
 	forever begin
 		repeat(2)@(vif.monitor_cb);
-			//capture();
-		if(both_op() &&(vif.monitor_cb.INP_VALID==2'b01 || vif.monitor_cb.INP_VALID==2'b10)) begin //both_op -01-10
-			if(vif.monitor_cb.MODE==1 &&(vif.monitor_cb.CMD==9 || vif.monitor_cb.CMD==10)) begin
-				repeat(2)@(vif.monitor_cb);
-				capture_in();
-				capture_out();
+			capture_in();
+		if(both_op() &&(mon_item.INP_VALID==2'b01 || mon_item.INP_VALID==2'b10)) begin //both_op -01-10
+			if(mon_item.MODE==1 &&(mon_item.CMD==9 || mon_item.CMD==10)) begin
+				//$display("enter if MUL");
+				//repeat(2)@(vif.monitor_cb);
+				//$display("MON_MUL");
+				//capture_in();
+				//capture_out();
+				//$display("\n\n IN WRITE MON 1 \n\n");
+				//mon_port.write(mon_item);
 			end
 			else begin
-				repeat(1)@(vif.monitor_cb);
-				capture_in();
-				capture_out();
+				//repeat(1)@(vif.monitor_cb);
+				//$display("enter if other");
+				//$display("MON others");
+				//capture_in();
+				//capture_out();
+				//$display("\n\n IN WRITE MON 2 \n\n");
+				//mon_port.write(mon_item);
 			end
-			for(int i=1; i<16; i++) begin 
-				repeat(1) @(vif.monitor_cb);
-				if(vif.monitor_cb.MODE==1 &&(vif.monitor_cb.CMD==9 || vif.monitor_cb.CMD==10)) begin
+			for(int i=1; i<16; i++) begin
+				repeat(1) @(vif.monitor_cb); 
+				capture_in();
+				/*
+				if(mon_item.MODE==1 &&(mon_item.CMD==9 || mon_item.CMD==10)) begin
 					repeat(1) @(vif.monitor_cb);
 				end
-			
-				if(vif.monitor_cb.INP_VALID!=2'b11) begin //not 11
-					if(vif.monitor_cb.MODE==1 &&(vif.monitor_cb.CMD==9 || vif.monitor_cb.CMD==10)) begin repeat(2)@(vif.monitor_cb); end
-					else begin repeat(1) @(vif.monitor_cb); end
-					
-					`uvm_info("MON :", $sformatf("INP_VALID is NOT 2'b11"), UVM_LOW)
+				else repeat(0)@(vif.monitor_cb);
+				*/
+				if(mon_item.INP_VALID!=2'b11) begin //not 11
 					continue;
 				end // not 11
 				else begin // got 11
+					$display("mon got 11");
 					if(vif.monitor_cb.MODE==1 &&(vif.monitor_cb.CMD==9 || vif.monitor_cb.CMD==10)) begin
 						repeat(2)@(vif.monitor_cb);
-						capture_in();
+						`uvm_info("MON :", $sformatf("RESET = %b | CE = %b| MODE = %b| CMD = %0d| INP_VALID = %0d| OPA = %0d| OPB = %0d| CIN = %b", mon_item.RESET, mon_item.CE, mon_item.MODE, mon_item.CMD, mon_item.INP_VALID, mon_item.OPA, mon_item.OPB, mon_item.CIN), UVM_LOW)
 						capture_out();
 						mon_port.write(mon_item);
 					end
 					else begin
 						repeat(1)@(vif.monitor_cb);
-						capture_in();
+						`uvm_info("MON :", $sformatf("RESET = %b | CE = %b| MODE = %b| CMD = %0d| INP_VALID = %0d| OPA = %0d| OPB = %0d| CIN = %b", mon_item.RESET, mon_item.CE, mon_item.MODE, mon_item.CMD, mon_item.INP_VALID, mon_item.OPA, mon_item.OPB, mon_item.CIN), UVM_LOW)
 						capture_out();
 						mon_port.write(mon_item);
 					end
-						$display("\t HIT BREAK mon : other");
 					repeat(1)@(vif.monitor_cb);
 					break;	
 				end  // got 11
@@ -104,13 +124,15 @@ class alu_monitor extends uvm_monitor;
 		else begin //other conditions
 			if(vif.monitor_cb.MODE==1 &&(vif.monitor_cb.CMD==9 || vif.monitor_cb.CMD==10)) begin
 				repeat(2)@(vif.monitor_cb);
-				capture_in();
+
+				`uvm_info("MON :", $sformatf("RESET = %b | CE = %b| MODE = %b| CMD = %0d| INP_VALID = %0d| OPA = %0d| OPB = %0d| CIN = %b", mon_item.RESET, mon_item.CE, mon_item.MODE, mon_item.CMD, mon_item.INP_VALID, mon_item.OPA, mon_item.OPB, mon_item.CIN), UVM_LOW)
 				capture_out();
 				mon_port.write(mon_item);
 			end
 			else begin
 				repeat(1)@(vif.monitor_cb);
-				capture_in();
+
+				`uvm_info("MON :", $sformatf("RESET = %b | CE = %b| MODE = %b| CMD = %0d| INP_VALID = %0d| OPA = %0d| OPB = %0d| CIN = %b", mon_item.RESET, mon_item.CE, mon_item.MODE, mon_item.CMD, mon_item.INP_VALID, mon_item.OPA, mon_item.OPB, mon_item.CIN), UVM_LOW)
 				capture_out();
 				mon_port.write(mon_item);
 			end
