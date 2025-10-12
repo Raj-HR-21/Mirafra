76ffc314bb1b43d81653a2afcc4ba462a53af394
Raj-HR-21 <rajendrahr21@gmail.com>
2025-10-12 22:22:15 +0530

    Update asy_fifo_assertion.sv

--- Asynchronous_fifo/src/asy_fifo_assertion.sv
+++ Asynchronous_fifo/src/asy_fifo_assertion.sv
@@ -1,20 +1,15 @@
-program write_assertion(wclk, wrst_n,winc, wfull, wdata);
+program assertions(wclk, wrst_n,winc, wfull, wdata, rdata, rempty, rinc, rclk, rrst_n);
 
         input wclk, wrst_n,winc, wfull;
         input [`DSIZE-1 : 0]wdata;
+        input rclk, rrst_n, rinc, rempty;
+        input [`DSIZE-1 : 0] rdata;
 
         property p1; // write reset check
                 @(posedge wclk) !wrst_n |-> !wfull;
         endproperty
         a1: assert property(p1)
         else $warning("Assertion fail : wrst_n : %b |  wfull : %b ", wrst_n, wfull);
-
-endprogram :write_assertion
-
-program read_assertion(rdata, rempty, rinc, rclk, rrst_n);
-
-        input rclk, rrst_n, rinc, rempty;
-        input [`DSIZE-1 : 0] rdata;
 
         property p2; //read reset check
                 @(posedge rclk) (!rrst_n) |-> (rempty );
@@ -29,4 +24,5 @@ program read_assertion(rdata, rempty, rinc, rclk, rrst_n);
         a3: assert property(p3)
 	else $warning("Assertion fail : when rinc = 0, rdata is not latched");
 
-endprogram :read_assertion
+endprogram :assertions
+
