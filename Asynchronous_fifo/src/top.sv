b0bbc370ae27096f61eda52032bc26e1b29a5467
Raj-HR-21 <rajendrahr21@gmail.com>
2025-10-12 22:32:11 +0530

    Update top.sv

--- Asynchronous_fifo/src/top.sv
+++ Asynchronous_fifo/src/top.sv
@@ -1,3 +1,4 @@
+
 `include "uvm_macros.svh"
 `include "uvm_pkg.sv"
 `include "defines.sv"
@@ -11,22 +12,26 @@ module top;
 	import asy_fifo_pkg::*;
 
 	bit wclk, rclk, wrst_n, rrst_n;
-	wr_interface wintf(wclk, wrst_n);
-	rd_interface rintf(rclk, rrst_n);
+	asy_fifo_interface intf(wclk, wrst_n, rclk, rrst_n);
 
 	fifo #(.DSIZE(`DSIZE), .ASIZE(`ASIZE))
-	dut(.wclk(wclk), .wrst_n(wrst_n), .wdata(wintf.wdata), .wfull(wintf.wfull), .winc(wintf.winc),.rclk(rclk), .rrst_n(rrst_n), .rdata(rintf.rdata), .rinc(rintf.rinc), .rempty(rintf.rempty));
+	dut(.wclk(wclk), .wrst_n(wrst_n), .wdata(intf.wdata), .wfull(intf.wfull), .winc(intf.winc),.rclk(rclk), .rrst_n(rrst_n), .rdata(intf.rdata), .rinc(intf.rinc), .rempty(intf.rempty));
 
-	bind wintf write_assertion wa(.wclk(wclk), .wrst_n(wrst_n), .winc(winc), .wfull(wfull), .wdata(wdata));
-	bind rintf read_assertion ra(.rclk(rclk), .rrst_n(rrst_n), .rinc(rinc), .rempty(rempty), .rdata(rdata));
+	bind fifo assertions wa(.wclk(wclk), .wrst_n(wrst_n), .winc(winc), .wfull(wfull), .wdata(wdata), .rclk(rclk), .rrst_n(rrst_n), .rinc(rinc), .rempty(rempty), .rdata(rdata));
 
 	always #5 wclk = ~wclk;
 	always #10 rclk = ~rclk;
 	initial begin
 		wclk = 0;
 		rclk = 0;
+
 		wrst_n = 0;
 		rrst_n = 0;
+	        intf.winc = 0;
+	        intf.rinc = 0;
+		#10;
+		wrst_n = 1;
+		rrst_n = 1;
 		#10;
 		wrst_n = 1;
 		rrst_n = 1;
@@ -37,14 +42,29 @@ module top;
 		#100;
 		wrst_n = 1;
 		rrst_n = 1;
-
+/*
+		#100;
+		wrst_n = 0;
+		rrst_n = 0;
+		#100;
+		wrst_n = 1;
+		rrst_n = 1;
+*/
 
 	end
 
 	initial begin
-		uvm_config_db#(virtual wr_interface)::set(null, "*", "wintf", wintf);
-		uvm_config_db#(virtual rd_interface)::set(null, "*", "rintf", rintf);
+		uvm_config_db#(virtual asy_fifo_interface)::set(null, "*", "intf", intf);
+
 		run_test("base_test");
+	//	run_test("wr_test");
+	//	run_test("rd_test");
+	//	run_test("wr_test1");
+	//	run_test("rd_test1");
+	//	run_test("wr_test2");
+	//	run_test("rd_test2");
+	//	run_test("continuous_write_then_read");
+	//	run_test("random_winc_rinc");
 	end
 
 	initial begin
